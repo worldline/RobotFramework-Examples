b1efe7169e2d6bd3ada06354b252d3f2068b345c
a480438 <vernon.crabtree@equensworldline.com>
2021-12-01 17:32:25 +0100

    Add some pypi details

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup
 setup(
     name="RobotFramework-Examples",
     version="0.0.1",
+    author="Vernon Crabtree",
+    author_email="vernon.b.crabtree@gmail.com",
     description="Examples in Robot Framework. Expands example data to individual test cases",
     packages=['.'],
     python_requires='>=3.8',
+    project_urls={
+        "Examples": "https://github.com/worldline/RobotFramework-Examples",
+    },    
     install_requires=['robotframework', 'pandas', 'sqlalchemy']
 )
