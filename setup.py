f2e7a22c495573305a68560b40cd07df0d700530
a480438 <vernon.crabtree@equensworldline.com>
2021-12-01 20:13:21 +0100

    Updates to metadata

--- setup.py
+++ setup.py
@@ -3,13 +3,32 @@ from setuptools import setup
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+CLASSIFIERS = '''
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3 :: Only
+Topic :: Software Development :: Testing
+Framework :: Robot Framework
+Framework :: Robot Framework :: Library
+'''.strip().splitlines()
+
 setup(
     name="RobotFramework-Examples",
-    version="0.0.5",
+    version="0.0.6",
     author="Vernon Crabtree",
     author_email="vernon.b.crabtree@gmail.com",
     description="Examples in Robot Framework. Expands example data to individual test cases",
     long_description=long_description,
+    license='Apache License 2.0',
+    keywords='robotframework testing testautomation bdd gwen examples',
+    platforms='any',
+    classifiers=CLASSIFIERS,
     packages=['.'],
     python_requires='>=3.8',
     project_urls={
