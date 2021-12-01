28250302dfd5a1739ca66d0314b54de0a8aeed9f
a480438 <vernon.crabtree@equensworldline.com>
2021-12-01 17:41:24 +0100

    Long description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="RobotFramework-Examples",
     version="0.0.2",
     author="Vernon Crabtree",
     author_email="vernon.b.crabtree@gmail.com",
     description="Examples in Robot Framework. Expands example data to individual test cases",
+    long_description=long_description,
     packages=['.'],
     python_requires='>=3.8',
     project_urls={
